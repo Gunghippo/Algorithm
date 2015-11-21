d316d61f4975bc0b88905d072eeeea388075c24e
Gunghippo <kung4019@gmail.com>
2015-11-21 07:32:25 +0700

    BinaryTree
    BinaryTree_Calculator

---
+++ BinaryTree_Calculator/BinaryTreeCalculator/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BinaryTreeCalculator
+{
+    partial class BinaryTree
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TreeViewData = new System.Windows.Forms.TreeView();
+            this.GenerateTreeButton = new System.Windows.Forms.Button();
+            this.TxtData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TreeViewData
+            // 
+            this.TreeViewData.Location = new System.Drawing.Point(12, 74);
+            this.TreeViewData.Name = "TreeViewData";
+            this.TreeViewData.Size = new System.Drawing.Size(196, 150);
+            this.TreeViewData.TabIndex = 0;
+            this.TreeViewData.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeViewData_AfterSelect);
+            // 
+            // GenerateTreeButton
+            // 
+            this.GenerateTreeButton.Location = new System.Drawing.Point(226, 30);
+            this.GenerateTreeButton.Name = "GenerateTreeButton";
+            this.GenerateTreeButton.Size = new System.Drawing.Size(93, 23);
+            this.GenerateTreeButton.TabIndex = 1;
+            this.GenerateTreeButton.Text = "Generate Tree";
+            this.GenerateTreeButton.UseVisualStyleBackColor = true;
+            this.GenerateTreeButton.Click += new System.EventHandler(this.GenerateTreeButton_Click);
+            // 
+            // TxtData
+            // 
+            this.TxtData.Location = new System.Drawing.Point(12, 30);
+            this.TxtData.Name = "TxtData";
+            this.TxtData.Size = new System.Drawing.Size(196, 20);
+            this.TxtData.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input Operad and Operation";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(12, 252);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 13);
+            this.ResultLabel.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Result:";
+            // 
+            // BinaryTree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 294);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtData);
+            this.Controls.Add(this.GenerateTreeButton);
+            this.Controls.Add(this.TreeViewData);
+            this.Name = "BinaryTree";
+            this.Text = "BirnaryTree";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TreeViewData;
+        private System.Windows.Forms.Button GenerateTreeButton;
+        private System.Windows.Forms.TextBox TxtData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BinaryTree_Calculator/BinaryTreeCalculator/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryTreeCalculator
+{
+    public partial class BinaryTree : Form
+    {
+        public BinaryTree()
+        {
+            InitializeComponent();
+        }
+
+
+        public int DataTextLength;
+        public string Plus = "+";
+        public string Menus = "-";
+        public string Muti = "*";
+        public string Div = "/";
+
+        private void GenerateTreeButton_Click(object sender, EventArgs e)
+        {
+            if (TreeViewData != null) TreeViewData.Nodes.Clear();
+            if (string.IsNullOrEmpty(TxtData.Text)) return;
+            GenerateTreeNode(inputText: TxtData.Text.Trim());
+        }
+
+
+        public void GenerateTreeNode(string inputText)
+        {
+            if (inputText == null) return;
+            DataTextLength = inputText.Length - 1;
+            var treeNode = CreateTreeNode(inputText: inputText);
+            TreeViewData.Nodes.Add(treeNode);
+        }
+
+        private TreeNode CreateTreeNode(string inputText)
+        {
+            if (DataTextLength < 0) return null;
+            if (!Plus.Contains(inputText[DataTextLength]) && !Menus.Contains(inputText[DataTextLength]) &&
+                !Muti.Contains(inputText[DataTextLength]) && !Div.Contains(inputText[DataTextLength]))
+                return new TreeNode(text: inputText[DataTextLength].ToString());
+            var character = inputText[DataTextLength];
+            DataTextLength -= 1;
+            var treeLeft = CreateTreeNode(inputText: inputText);
+            DataTextLength -= 1;
+            var treeRight = CreateTreeNode(inputText: inputText);
+            TreeNode[] node = null;
+            if (treeLeft == null)
+            {
+                if (treeRight != null) node = new[] { treeRight };
+            }
+            else if (treeRight == null)
+                node = new[] { treeLeft };
+            else
+                node = new[] { treeLeft, treeRight };
+            return node != null ? new TreeNode(character.ToString(), node) : null;
+        }
+
+        private void TreeViewData_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (ResultLabel == null) return;
+            ResultLabel.Text = string.Empty;
+            var nodeName = TreeViewData.SelectedNode.Text;
+            if (Plus.Contains(nodeName) || Menus.Contains(nodeName) ||
+                Muti.Contains(nodeName) || Div.Contains(nodeName))
+            {
+                var node = TreeViewData.SelectedNode;
+                nodeName = FindChildNode(node);
+                nodeName = nodeName.Length < 6 ? nodeName.Replace('(', ' ').Replace(')', ' ').Trim() : nodeName.Substring(1, length: nodeName.Length - 2);
+                var result = CalculateDataInNode(selectedNode: TreeViewData.SelectedNode);
+                nodeName = nodeName + " = " + result;
+                ResultLabel.Text = nodeName;
+            }
+            else ResultLabel.Text = nodeName;
+        }
+
+        private double CalculateDataInNode(TreeNode selectedNode)
+        {
+            var nodeName = selectedNode.Text;
+            var amountOfNode = selectedNode.GetNodeCount(false);
+
+            if (amountOfNode <= 0 ||
+                (Plus.Contains(nodeName) && Menus.Contains(nodeName) && Muti.Contains(nodeName) &&
+                 Div.Contains(nodeName))) return Convert.ToDouble(nodeName);
+            switch (Convert.ToChar(nodeName))
+            {
+                case '+': return CalculateDataInNode(selectedNode: selectedNode.Nodes[1]) + CalculateDataInNode(selectedNode.Nodes[0]);
+                case '-': return CalculateDataInNode(selectedNode: selectedNode.Nodes[1]) - CalculateDataInNode(selectedNode.Nodes[0]);
+                case '*': return CalculateDataInNode(selectedNode: selectedNode.Nodes[1]) * CalculateDataInNode(selectedNode.Nodes[0]);
+                case '/': return CalculateDataInNode(selectedNode: selectedNode.Nodes[1]) / CalculateDataInNode(selectedNode.Nodes[0]);
+                default: return 0;
+            }
+            return Convert.ToDouble(nodeName);
+        }
+
+        private static string FindChildNode(TreeNode selectedNode)
+        {          
+            var nodeName = selectedNode.Text;
+            var amountOfNode = selectedNode.GetNodeCount(false);
+            if (amountOfNode > 0)
+                return "(" + FindChildNode(selectedNode.Nodes[1]) + nodeName + FindChildNode(selectedNode.Nodes[0]) + ")";
+            return nodeName;
+        }
+
+       
+    }
+}
